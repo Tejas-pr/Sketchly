896adcb9e92dbdb4813aa80c1e17871e1941af1c
Tejas <tejas.teju02@gmail.com>
2025-10-11 19:23:49 +0530

    chang sourcve

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,9 +1,5 @@
 import { createAuthClient } from "better-auth/react";
-import { toNextJsHandler } from "better-auth/next-js";
-import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
-
-export const nextjsHandler = toNextJsHandler(auth);
