7929549a072e493c6c18395028e619795d792a2d
Tejas <tejas.teju02@gmail.com>
2025-10-17 10:48:10 +0530

    prisma fix 11

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,8 @@
-// apps/web/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    esmExternals: 'loose',
+    esmExternals: "loose",
   },
 };
 
