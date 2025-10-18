930f6800af22cb5b8959c295f545459d3eaf0061
Tejas <tejas.teju02@gmail.com>
2025-10-18 10:39:05 +0530

    fixxxxxxx

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,17 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
+    devIndicators: false,
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
