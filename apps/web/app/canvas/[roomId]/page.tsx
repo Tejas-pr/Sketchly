142642fd2b1be8ba4139ca7462511a558861c609
Tejas <tejas.teju02@gmail.com>
2025-10-19 21:27:36 +0530

    underConstrution page

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,11 +1,17 @@
 "use server";
 
 import Canvas from "@/components/Canvas";
+import { UnderConstruction } from "@/components/under-construction";
 import { MyPageProps } from "@/lib/types";
 
 export default async function CanvasPage({ params }: MyPageProps) {
   const resolvedParams = await params;
   const roomId = resolvedParams.roomId;
 
-  return <Canvas roomId={roomId} />;
+  return (
+    <>
+      {/* <Canvas roomId={roomId} /> */}
+      <UnderConstruction />
+    </>
+  );
 }
